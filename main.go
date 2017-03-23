f4b916cff29cbbe13557538e8fea00d0873b43bd
Serge Catudal <serge.catudal@gmail.com>
2017-03-23 07:04:58 -0400

    Combine all const in the same block

--- main.go
+++ main.go
@@ -45,14 +45,11 @@ import (
 )
 
 const (
+	dockerCfgTemplate                = `{"%s":{"username":"oauth2accesstoken","password":"%s","email":"none"}}`
+	dockerJSONTemplate               = `{"auths":{"%s":{"auth":"%s","email":"none"}}}`
 	dockerPrivateRegistryPasswordKey = "DOCKER_PRIVATE_REGISTRY_PASSWORD"
 	dockerPrivateRegistryServerKey   = "DOCKER_PRIVATE_REGISTRY_SERVER"
 	dockerPrivateRegistryUserKey     = "DOCKER_PRIVATE_REGISTRY_USER"
-)
-
-const (
-	dockerCfgTemplate  = `{"%s":{"username":"oauth2accesstoken","password":"%s","email":"none"}}`
-	dockerJSONTemplate = `{"auths":{"%s":{"auth":"%s","email":"none"}}}`
 )
 
 var (
